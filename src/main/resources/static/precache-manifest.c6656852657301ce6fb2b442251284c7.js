9afcb289d992891afb2e1ab440066d1457b149ee
Mauricio Cavallini <mauricio1acp@hotmail.com>
2020-05-16 22:15:38 +0200

    front-end react

---
+++ src/main/resources/static/precache-manifest.c6656852657301ce6fb2b442251284c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03fad508d8f6e8d146f0229b228189d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b02dad0ea7142b9048bd",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "02da8608a865042fe5c8",
+    "url": "/static/css/main.86ded62d.chunk.css"
+  },
+  {
+    "revision": "b02dad0ea7142b9048bd",
+    "url": "/static/js/2.33bfeeab.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.33bfeeab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02da8608a865042fe5c8",
+    "url": "/static/js/main.13fea6b9.chunk.js"
+  },
+  {
+    "revision": "165f034ffb316832c97f",
+    "url": "/static/js/runtime-main.86e74220.js"
+  }
+]);
